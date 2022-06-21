93db598a51ed5b4b896b5d78fc5812590e2ab60e
Lê Quang Minh <106050463+minhlqBHsoft@users.noreply.github.com>
2022-06-21 09:01:20 +0700

    demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import image from '../../figma-react/my-app/src/img/Logo.png'
 
 function App() {
   return (
     <div className="App">
       <h1>123 ducks on the tree, im your daddy</h1>
-      <img src={image} alt="" />
     </div>
   );
 }
